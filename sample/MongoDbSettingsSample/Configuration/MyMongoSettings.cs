0d82e0fd4f5bfd79410abe38a273c732ad4125a9
Anthony Sneed <anthony.sneed@hilti.com>
2022-08-04 19:24:29 -0500

    Provide DI registration for IMongoDatabase and IDocumentUnitOfWork.
    - Add MongoDbSettings.
    - Add ServiceLifetime parameter to AddMongoDbSettings methods.
    - Add AddMongoDbTransactionSettings method.

--- sample/MongoDbSettingsSample/Configuration/MyMongoSettings.cs
+++ sample/MongoDbSettingsSample/Configuration/MyMongoSettings.cs
@@ -2,9 +2,6 @@ using EventDriven.DependencyInjection.URF.Mongo;
 
 namespace MongoDbSettingsSample.Configuration;
 
-public class MyMongoSettings : IMongoDbSettings
+public class MyMongoSettings : MongoDbSettings
 {
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string CollectionName { get; set; } = null!;
 }
--- src/EventDriven.DependencyInjection.URF.Mongo/ServiceCollectionExtensions.cs
+++ src/EventDriven.DependencyInjection.URF.Mongo/ServiceCollectionExtensions.cs
@@ -16,39 +16,97 @@ namespace EventDriven.DependencyInjection.URF.Mongo
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
         /// <param name="config">The application's <see cref="IConfiguration"/>.</param>
+        /// <param name="lifetime">Service lifetime.</param>
         /// <typeparam name="TAppSettings">Strongly typed app settings class.</typeparam>
         /// <typeparam name="TEntity">Entity type.</typeparam>
         /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddMongoDbSettings<TAppSettings, TEntity>(
-            this IServiceCollection services, IConfiguration config)
+            this IServiceCollection services, IConfiguration config,
+            ServiceLifetime lifetime = ServiceLifetime.Singleton)
             where TAppSettings : class, IMongoDbSettings
             where TEntity : class =>
-            services.AddMongoDbSettings<TAppSettings, TEntity, TEntity>(config);
+            services.AddMongoDbSettings<TAppSettings, TEntity, TEntity>(config, lifetime);
 
         /// <summary>
         /// Register MongoDB collection with dependency injection.
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
         /// <param name="config">The application's <see cref="IConfiguration"/>.</param>
+        /// <param name="lifetime">Service lifetime.</param>
         /// <typeparam name="TAppSettings">Strongly typed app settings class.</typeparam>
         /// <typeparam name="TEntity">Entity type.</typeparam>
         /// <typeparam name="TDocumentEntity">Document entity type.</typeparam>
         /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddMongoDbSettings<TAppSettings, TEntity, TDocumentEntity>(
-            this IServiceCollection services, IConfiguration config)
+            this IServiceCollection services, IConfiguration config,
+            ServiceLifetime lifetime = ServiceLifetime.Singleton)
             where TAppSettings : class, IMongoDbSettings
             where TEntity : class
             where TDocumentEntity : class
         {
-            services.AddAppSettings<TAppSettings>(config);
-            services.AddSingleton(sp =>
+            IMongoCollection<TEntity> GetMongoCollection(IServiceProvider sp)
             {
                 var settings = sp.GetRequiredService<TAppSettings>();
                 var client = new MongoClient(settings.ConnectionString);
                 var database = client.GetDatabase(settings.DatabaseName);
                 return database.GetCollection<TEntity>(settings.CollectionName);
-            });
-            services.AddSingleton<IDocumentRepository<TDocumentEntity>, DocumentRepository<TDocumentEntity>>();
+            }
+            services.AddAppSettings<TAppSettings>(config, lifetime);
+            switch (lifetime)
+            {
+                case ServiceLifetime.Transient:
+                    services.AddTransient(GetMongoCollection);
+                    services.AddTransient<IDocumentRepository<TDocumentEntity>, DocumentRepository<TDocumentEntity>>();
+                    break;
+                case ServiceLifetime.Scoped:
+                    services.AddScoped(GetMongoCollection);
+                    services.AddScoped<IDocumentRepository<TDocumentEntity>, DocumentRepository<TDocumentEntity>>();
+                    break;
+                default:
+                    services.AddSingleton(GetMongoCollection);
+                    services.AddSingleton<IDocumentRepository<TDocumentEntity>, DocumentRepository<TDocumentEntity>>();
+                    break;
+            }
+            return services;
+        }
+
+        /// <summary>
+        /// Register MongoDB database with dependency injection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="config">The application's <see cref="IConfiguration"/>.</param>
+        /// <param name="lifetime">Service lifetime.</param>
+        /// <typeparam name="TAppSettings">Strongly typed app settings class.</typeparam>
+        /// <typeparam name="TDocumentUnitOfWork">Document unit of work.</typeparam>
+        /// <returns></returns>
+        public static IServiceCollection AddMongoDbTransactionSettings<TAppSettings, TDocumentUnitOfWork>(
+            this IServiceCollection services, IConfiguration config,
+            ServiceLifetime lifetime = ServiceLifetime.Singleton)
+            where TAppSettings : class, IMongoDbSettings
+            where TDocumentUnitOfWork : class, IDocumentUnitOfWork
+        {
+            IMongoDatabase GetMongoDatabase(IServiceProvider sp)
+            {
+                var settings = sp.GetRequiredService<TAppSettings>();
+                var client = new MongoClient(settings.ConnectionString);
+                return client.GetDatabase(settings.DatabaseName);
+            }
+            services.AddAppSettings<TAppSettings>(config, lifetime);
+            switch (lifetime)
+            {
+                case ServiceLifetime.Transient:
+                    services.AddTransient(GetMongoDatabase);
+                    services.AddTransient<IDocumentUnitOfWork, TDocumentUnitOfWork>();
+                    break;
+                case ServiceLifetime.Scoped:
+                    services.AddScoped(GetMongoDatabase);
+                    services.AddScoped<IDocumentUnitOfWork, TDocumentUnitOfWork>();
+                    break;
+                default:
+                    services.AddSingleton(GetMongoDatabase);
+                    services.AddSingleton<IDocumentUnitOfWork, TDocumentUnitOfWork>();
+                    break;
+            }
             return services;
         }
     }
